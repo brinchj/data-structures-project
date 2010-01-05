17050461bf43b04798e330c67f9a7fef833c249e
Johan Brinch <zerrez@gmail.com>
2010-01-05 15:14:30 +0100

    cleanup

--- src/scripts/cpp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-for i in *.c++; do
-	cp ${i} cphstl/;
-	g++ -O3 cphstl/${i} -o ${i}.o || exit 1;
-done
--- src/scripts/hpp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-for i in *.h++; do
-	cp ${i} cphstl/;
-	g++ -O3 cphstl/${i} || exit 1;
-done
